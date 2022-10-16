ed13537a8f01786c0d935007999d278a0da912d4
bhargav <102895435+bhargavsai259@users.noreply.github.com>
2022-10-16 16:05:43 +0530

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,51 @@
+/* js  multi line comment comments  */
+
+
+// single line comment
+
+
+//arrays
+
+// const cars = [];
+// cars[0]= "Saab";
+// cars[1]= "Volvo";
+// cars[2]= "BMW";
+// console.log(cars[0]);
+
+//or 
+
+ //const cars = new Array("Saab", "Volvo", "BMW");
+
+// console.log(cars);
+
+//functions
+
+// cars.length
+
+// cars.sort()
+
+// console.log(cars);
+
+
+
+//  for looping over
+
+
+/*
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let fLen = fruits.length;
+
+let text = " ";
+for (let i = 0; i < fLen; i++) {
+  text += " " + fruits[i] + " ";
+}
+text += " ";
+
+console.log(text);*/
+
+
+//to insert elemnts
+
+//frits.push("lemon");
+
+
